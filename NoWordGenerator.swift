7f527ced5081e3fa580bd6a72c1d64e21e63b29c
Sara Buccino <sara.sara.997@gmail.com>
2018-02-13 12:21:03 +0100

    a

--- NoWordGenerator.swift
+++ NoWordGenerator.swift
@@ -11,15 +11,20 @@ import UIKit
 class NoWordGenerator: UIViewController {
     var characterNumber : Int = 3
     var position = 1
+    var fonemaInizialeLegth = 0
     
     @IBOutlet weak var fonemaIniziale: UITextField!
     @IBOutlet weak var parolaGenerata: UILabel!
     
     @IBOutlet weak var wordLengthView: UILabel!
-    @IBAction func wordLength(_ sender: UISlider) {
+    
+    @IBAction func wordLegthStepper(_ sender: UIStepper) {
         characterNumber = Int(sender.value)
         wordLengthView.text = String(characterNumber)
     }
+    
+    @IBOutlet weak var legthStepper: UIStepper!
+    
     
     //Imposto le lettere da usare
     let lettere = [ "r", "t", "p", "s", "d", "f", "g", "l", "z", "c", "v", "b", "n", "m"]
@@ -57,15 +62,28 @@ class NoWordGenerator: UIViewController {
         
         inizioLabel.textColor = UIColor.blue
         
-        parolaGenerata.text = " "    }
+        parolaGenerata.text = " "
+        
+        
+        //characterNumber = Int(legthStepper.value)
+        //fonemaInizialeLegth = (fonemaIniziale.text?.count)!
+        print("\(fonemaInizialeLegth)aa")
+        
+        print("AA \(characterNumber)")
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
     @IBAction func generationStart(_ sender: Any) {
         generazione()
         generationAndCheck()
+        
         
         // var w = lunghezzaSlider.value
         
@@ -111,13 +129,54 @@ class NoWordGenerator: UIViewController {
             
             
             
-            
+            fonemaInizialeLegth = (fonemaIniziale.text?.count)!
             let fChar = fonemaIniziale.text?.first
             let lastChar = fonemaIniziale.text?.last
             
-            //if phonemSwitch.isOn {
+            
             if position == 2 {
                 
+                
+                
+                //controlla se il fonema inserito è lungo  uno o due caratteri in meno alla lunghezza della parola lo centra meglio
+                
+                
+                if fonemaInizialeLegth == characterNumber - 1 || fonemaInizialeLegth == characterNumber - 2 {
+                
+                    
+                    
+                    if vocali.contains("\(fChar!)"){
+                        if vocali.contains("\(lastChar!)"){
+                            parolaGenerata.text = l11 + fonemaIniziale.text! + l1 + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10
+                            
+                        } else {
+                            parolaGenerata.text = l11 + fonemaIniziale.text! + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10 + l1
+                            
+                        }
+                        
+                        
+                    } else {
+                        
+                        if vocali.contains("\(lastChar!)"){
+                            
+                            
+                            
+                            parolaGenerata.text = l12 + fonemaIniziale.text! + l1 + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10
+                            
+                        } else {
+                            parolaGenerata.text = l12 + fonemaIniziale.text! + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10 + l1
+                            
+                        }
+                        
+                    }
+                    
+                } else {
+                    
+                    
+                
+                //fine controllo
+                    
+                    
                 if vocali.contains("\(fChar!)"){
                     if vocali.contains("\(lastChar!)"){
                         parolaGenerata.text = l11 + fonemaIniziale.text! + l1 + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10
@@ -132,6 +191,8 @@ class NoWordGenerator: UIViewController {
                     
                     if vocali.contains("\(lastChar!)"){
                         
+                        
+                        
                         parolaGenerata.text = l11 + l12 + fonemaIniziale.text! + l1 + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10
                         
                     } else {
@@ -139,6 +200,7 @@ class NoWordGenerator: UIViewController {
                         
                     }
                     
+                }
                 }
                 
             }else if position == 1 {
