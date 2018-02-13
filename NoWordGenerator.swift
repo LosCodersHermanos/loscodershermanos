e9c4a8d9c7cffa4d91b86a9b59940155ece98202
Sara Buccino <sara.sara.997@gmail.com>
2018-02-13 11:06:22 +0100

    aa

--- NoWordGenerator.swift
+++ NoWordGenerator.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class NoWordGenerator: UIViewController {
     var characterNumber : Int = 3
-
+    var position = 1
+    
     @IBOutlet weak var fonemaIniziale: UITextField!
     @IBOutlet weak var parolaGenerata: UILabel!
     
@@ -19,21 +20,45 @@ class NoWordGenerator: UIViewController {
         characterNumber = Int(sender.value)
         wordLengthView.text = String(characterNumber)
     }
-    @IBOutlet weak var phonemSwitch: UISwitch!
+    
     //Imposto le lettere da usare
     let lettere = [ "r", "t", "p", "s", "d", "f", "g", "l", "z", "c", "v", "b", "n", "m"]
     let vocali = ["a", "e", "i", "o", "u"]
     // Controllo parole conosciute
-
     
+    @IBAction func positionSlider(_ sender: UISlider) {
+        position = Int(sender.value)
+        
+        
+        if position == 1{
+            inizioLabel.textColor = UIColor.blue
+            centroLabel.textColor = UIColor.black
+            fineLabel.textColor = UIColor.black
+        } else if position == 2 {
+            inizioLabel.textColor = UIColor.black
+            centroLabel.textColor = UIColor.blue
+            fineLabel.textColor = UIColor.black
+        } else {
+            inizioLabel.textColor = UIColor.black
+            centroLabel.textColor = UIColor.black
+            fineLabel.textColor = UIColor.blue
+        }
+        
+        
+    }
     
-    
+    @IBOutlet weak var inizioLabel: UILabel!
+    @IBOutlet weak var centroLabel: UILabel!
+    @IBOutlet weak var fineLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-parolaGenerata.text = " "    }
-
+        
+        
+        inizioLabel.textColor = UIColor.blue
+        
+        parolaGenerata.text = " "    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -54,115 +79,136 @@ parolaGenerata.text = " "    }
             
         } else {
             let MAX : UInt32 = 13
-            let random_number = Int(arc4random_uniform(MAX))
-            let random_number2 = Int(arc4random_uniform(MAX))
-            let random_number4 = Int(arc4random_uniform(MAX))
-            let random_number6 = Int(arc4random_uniform(MAX))
-            let random_number8 = Int(arc4random_uniform(MAX))
+            let random_number1 = Int(arc4random_uniform(MAX))
+            let random_number3 = Int(arc4random_uniform(MAX))
+            let random_number5 = Int(arc4random_uniform(MAX))
+            let random_number7 = Int(arc4random_uniform(MAX))
+            let random_number9 = Int(arc4random_uniform(MAX))
+            let random_number11 = Int(arc4random_uniform(MAX))
             
             let MAX1 : UInt32 = 5
-            let random_number1 = Int(arc4random_uniform(MAX1))
-            let random_number3 = Int(arc4random_uniform(MAX1))
-            let random_number5 = Int(arc4random_uniform(MAX1))
-            let random_number7 = Int(arc4random_uniform(MAX1))
-            let random_number9 = Int(arc4random_uniform(MAX1))
+            let random_number2 = Int(arc4random_uniform(MAX1))
+            let random_number4 = Int(arc4random_uniform(MAX1))
+            let random_number6 = Int(arc4random_uniform(MAX1))
+            let random_number8 = Int(arc4random_uniform(MAX1))
+            let random_number10 = Int(arc4random_uniform(MAX1))
+            let random_number12 = Int(arc4random_uniform(MAX1))
             
             
-            let l1 = lettere[random_number]
-            let l2 = vocali[random_number1]
-            let l3 = lettere[random_number2]
-            let l4 = vocali[random_number3]
-            let l5 = lettere[random_number4]
-            let l6 = vocali[random_number5]
-            let l7 = lettere[random_number6]
-            let l8 = vocali[random_number7]
-            let l9 = lettere[random_number8]
-            let l10 = vocali[random_number9]
+            let l2 = vocali[random_number2]
+            let l4 = vocali[random_number4]
+            let l6 = vocali[random_number6]
+            let l8 = vocali[random_number8]
+            let l10 = vocali[random_number10]
+            let l12 = vocali[random_number12]
+            
+            let l1 = lettere[random_number1]
+            let l3 = lettere[random_number3]
+            let l5 = lettere[random_number5]
+            let l7 = lettere[random_number7]
+            let l9 = lettere[random_number9]
+            let l11 = lettere[random_number11]
+            
+            
+            
             
             let fChar = fonemaIniziale.text?.first
             let lastChar = fonemaIniziale.text?.last
-
-         if phonemSwitch.isOn {
             
-            if vocali.contains("\(fChar!)"){
-                if vocali.contains("\(lastChar!)"){
-                    parolaGenerata.text = l5 + fonemaIniziale.text! + l1 + l2 + l3 + l4 + l2 + l7 + l8 + l9 + l10
+            //if phonemSwitch.isOn {
+            if position == 2 {
+                
+                if vocali.contains("\(fChar!)"){
+                    if vocali.contains("\(lastChar!)"){
+                        parolaGenerata.text = l11 + fonemaIniziale.text! + l1 + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10
+                        
+                    } else {
+                        parolaGenerata.text = l11 + fonemaIniziale.text! + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10 + l1
+                        
+                    }
+                    
                     
                 } else {
-                    parolaGenerata.text = l5 + fonemaIniziale.text! + l2 + l3 + l4 + l1 + l8 + l9 + l10 + l7
+                    
+                    if vocali.contains("\(lastChar!)"){
+                        
+                        parolaGenerata.text = l11 + l12 + fonemaIniziale.text! + l1 + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10
+                        
+                    } else {
+                        parolaGenerata.text = l11 + l12 + fonemaIniziale.text! + l2 + l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10 + l1
+                        
+                    }
                     
                 }
                 
-                
-            } else {
+            }else if position == 1 {
                 
                 if vocali.contains("\(lastChar!)"){
-                    
-                    parolaGenerata.text = l5 + l6 + fonemaIniziale.text! + l1 + l2 + l3 + l4 + l7 + l8 + l9 + l10
+                    parolaGenerata.text = fonemaIniziale.text! + l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10
                     
                 } else {
-                    parolaGenerata.text = l5 + l6 + fonemaIniziale.text! + l2 + l3 + l4 + l1 + l8 + l9 + l10 + l7
+                    parolaGenerata.text = fonemaIniziale.text! + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l1
+                    
+                }
+            } else {
+                
+                if vocali.contains("\(fChar!)"){
+                    parolaGenerata.text = /*l1 + l2 + l3 +*/ l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + fonemaIniziale.text!
+                    
+                } else {
+                    parolaGenerata.text = /* l1 + l2 +*/ l3 +  l4 + l5 + l6 + l7 + l8 + l9 + l10 + fonemaIniziale.text!
                     
                 }
                 
             }
             
-        }else {
-            
-            if vocali.contains("\(lastChar!)"){
-                parolaGenerata.text = fonemaIniziale.text! + l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10
-                
-            } else {
-                parolaGenerata.text = fonemaIniziale.text! + l2 + l3 + l4 + l1 + l6 + l7 + l8 + l9 + l10 + l5
-                
-            }
         }
         
-    }
         return parolaGenerata.text!
-       
+        
     }
-            func generationAndCheck() -> String  {
+    func generationAndCheck() -> String  {
         let str = parolaGenerata.text!
         let index = str.index(str.startIndex , offsetBy: characterNumber)
         var trimStr = str.substring(to: index)
         
-    
-              var parole: String = ""
-                if let filepath = Bundle.main.path(forResource: "dizionario", ofType: "txt") {
+        
+        var parole: String = ""
+        if let filepath = Bundle.main.path(forResource: "dizionario", ofType: "txt") {
             do {
-             parole = try String(contentsOfFile: filepath)
+                parole = try String(contentsOfFile: filepath)
                 print(trimStr)
             } catch {
                 // contents could not be loaded
-//                    }
-//                    let test: [String] = parole.components(separatedBy: "\n")
-//                    guard test.contains(trimStr) else {
-//                        parolaGenerata.isHidden = false
-//                        parolaGenerata.text = trimStr
-//                        return trimStr
-//                    }
-//                    trimStr = ""
+                //                    }
+                //                    let test: [String] = parole.components(separatedBy: "\n")
+                //                    guard test.contains(trimStr) else {
+                //                        parolaGenerata.isHidden = false
+                //                        parolaGenerata.text = trimStr
+                //                        return trimStr
+                //                    }
+                //                    trimStr = ""
                 while parole.contains(trimStr){
                     print(trimStr)
                     trimStr = " "
-                   
+                    
                     self.generazione()
                     self.generationAndCheck()
                     
-//                                           let newTrimStr:String
-//                                           newTrimStr = generazione()
-//                                            parolaGenerata.text = newTrimStr
-                
+                    //                                           let newTrimStr:String
+                    //                                           newTrimStr = generazione()
+                    //                                            parolaGenerata.text = newTrimStr
+                    
                     parolaGenerata.text = trimStr
                     parolaGenerata.isHidden = true
                     
                 }
-    }
-                    parolaGenerata.text = trimStr
+            }
+            parolaGenerata.text = trimStr
+            return trimStr
+        }
         return trimStr
     }
-                return trimStr
-}
     
 }
+
